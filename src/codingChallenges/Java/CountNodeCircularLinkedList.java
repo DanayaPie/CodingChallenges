9cf5f3e0866fc3e7a27f9155e56980dde9b081f5
DanayaPie <92035671+DanayaPie@users.noreply.github.com>
2023-06-12 14:39:13 -0400

    number of nodes in circular linked list

--- src/codingChallenges/Java/CountNodeCircularLinkedList.java
+++ src/codingChallenges/Java/CountNodeCircularLinkedList.java
@@ -6,22 +6,72 @@ public class CountNodeCircularLinkedList {
 
     // given a circular linked list, count the number of nodes
 
-    CountCircularNode head;
+    CountCircularNode head = null;
+    CountCircularNode tail = null;
 
-    void push(int data) {
+    public void push(int data) {
+
+        CountCircularNode newNode = new CountCircularNode(data);
 
         if (head != null) {
 
-            CountCircularNode newNode = new CountCircularNode(data);
-            newNode.next = head.next;
-            head.next = newNode;
+            newNode.next = head;
             head = newNode;
+            tail.next = head;
+
         } else {
 
-            CountCircularNode newNode = new CountCircularNode(data);
             head = newNode;
-            head.next = head;
+            tail = newNode;
+            newNode.next = head;
         }
+    }
+
+    public void printList() {
+
+        CountCircularNode current = head;
+
+        if (head != null) {
+
+            do {
+                System.out.print(current.data + " -> ");
+                current = current.next;
+
+            } while (current != head);
+        }
+        System.out.println();
+    }
+
+    public int countNode() {
+
+        int count = 0;
+
+        if (head != null) {
+
+            CountCircularNode current = head;
+
+            while (current.next != head) {
+
+                current = current.next;
+                count++;
+            }
+
+            count++;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        CountNodeCircularLinkedList linkedList = new CountNodeCircularLinkedList();
+        linkedList.push(3);
+        linkedList.push(5);
+        linkedList.push(2);
+        linkedList.push(0);
+
+        linkedList.printList();
+        System.out.println("Number of node(s): " + linkedList.countNode());
     }
 }
 
