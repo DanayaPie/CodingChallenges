ada60d857903d756576dc4a56cc3211d3e6e55e3
DanayaPie <92035671+DanayaPie@users.noreply.github.com>
2023-06-25 17:16:44 -0400

    two sum

--- src/codingChallenges/Java/TwoSum.java
+++ src/codingChallenges/Java/TwoSum.java
@@ -8,28 +8,31 @@ public class TwoSum {
     // Assume there is exactly one solution, and you may not use the same element twice.
     public static void main(String[] args) {
 
-        int[] nums = {2, 9, 11, 15};
+        int[] nums = {2, 11, 15, 9};
         int target = 20;
 
-        System.out.println("first index: " + twoSum(nums, target)[0] + ", second index: " + twoSum(nums, target)[1]);
+        int[] arr = twoSum(nums, target);
+
+        System.out.println(arr[0] + ", " + arr[1]);
     }
 
-    private static int[] twoSum(int[] nums, int target) {
+    // hashMap
+    public static int[] twoSum(int[] nums, int target) {
 
-        HashMap<Integer, Integer> numMap = new HashMap<>();
+        HashMap<Integer, Integer> numsMap = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
 
             int diff = target - nums[i];
 
-            if (!numMap.containsKey(diff)) {
-                numMap.put(nums[i], i);
+            if (numsMap.containsKey(diff)) {
 
-            } else {
-                return new int[] {numMap.get(diff), i};
+                return new int[]{numsMap.get(diff), i};
             }
+
+            numsMap.put(nums[i], i);
         }
 
-        return new int[] {-1, -1};
+        return new int[]{-1, -1};
     }
 }
